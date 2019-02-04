98d7b2d1a5a3021d001e003105a3b5c46d4e2f7a
necrotric <johan_cool01@hotmail.com>
2019-02-04 05:10:52 +0100

    first commit

---
+++ app/src/main/java/necrotric/example/marvelhero/Controller/SeriesAcitvity.kt
@@ -0,0 +1,12 @@
+package necrotric.example.marvelhero.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SeriesAcitvity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_series_acitvity)
+    }
+}
