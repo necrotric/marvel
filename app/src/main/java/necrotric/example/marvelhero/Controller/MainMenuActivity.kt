f9cf548b845e2e5381a067a6b0d818a2ecb7152f
necrotric <johan_cool01@hotmail.com>
2019-02-06 18:12:42 +0100

    Made mainmenu activity

---
+++ app/src/main/java/necrotric/example/marvelhero/Controller/MainMenuActivity.kt
@@ -0,0 +1,13 @@
+package necrotric.example.marvelhero.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import necrotric.example.marvelhero.R
+
+class MainMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
