7b267fc3e5c2d01e9373bacdcee5bc2b24790d92
johan <johan_cool01@hotmail.com>
2019-02-12 13:12:38 +0100

    Refactored browseHero to fragments

--- app/src/main/java/necrotric/example/marvelhero/Controller/NewMain.kt
+++ app/src/main/java/necrotric/example/marvelhero/Controller/NewMain.kt
@@ -10,7 +10,7 @@ class NewMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_main)
-        navigateTo(SeriesBrowseFragment())
+        navigateTo(HeroBrowseFragment())
     }
     private fun navigateTo(fragment: Fragment){
         val transaction = supportFragmentManager.beginTransaction()
